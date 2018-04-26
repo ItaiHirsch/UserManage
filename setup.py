16502b821d7f21d0e33c32358cf1dd590ca35059
Itai Hirsch <itaihirsch@itais-mba.lan>
2018-04-26 15:21:38 +0300

    Making it a python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='UserManage',
+    version='1.0',
+    description='Common utils for Reblaze',
+    author='Ravid&Itai',
+    packages = ['UserManage'],
+    package_dir = {'UserManage': 'UserManage'},
+)
